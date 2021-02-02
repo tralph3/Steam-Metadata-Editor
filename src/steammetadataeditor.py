cc137bf40d2b96c3889f5e4e61dc8fb290918cd6
tralph3 <tomasralph2000@gmail.com>
2021-02-01 21:11:25 -0300

    Added LICENSE

--- src/steammetadataeditor.py
+++ src/steammetadataeditor.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
+#
+#A Metadata Editor for Steam Applications
+#Copyright (C) 2021  Tomás Ralph
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+##################################
+#                                #
+#       Created by tralph3       #
+#   https://github.com/tralph3   #
+#                                #
+##################################
+
 from hashlib import sha1
 import tkinter as tk
 from tkinter.ttk import Treeview, Style
