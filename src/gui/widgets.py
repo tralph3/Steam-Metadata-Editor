102767f7f7a0c93ae35aed1df7d9f72881d9fc6c
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-11-07 10:40:40 +0500

    Refactor `widgets.py`
    * Beautify imports
    * Use config's properties directly instead of creating temp global variables

--- src/gui/widgets.py
+++ src/gui/widgets.py
@@ -15,38 +15,23 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import tkinter as tk
-from config import config
 
-BG = config.BG
-BTTN_ACTIVE_BG = config.BTTN_ACTIVE_BG
-DLT_BTTN_BG = config.DLT_BTTN_BG
-DLT_BTTN_ACTIVE_BG = config.DLT_BTTN_ACTIVE_BG
-BTTN_ACTIVE_FG = config.BTTN_ACTIVE_FG
-BTTN_BG = config.BTTN_BG
-BTTN_FG = config.BTTN_FG
-BTTN_FONT = config.BTTN_FONT
-BTTN_FONT_SIZE = config.BTTN_FONT_SIZE
-BTTN_RELIEF = config.BTTN_RELIEF
-ENTRY_BG = config.ENTRY_BG
-ENTRY_FG = config.ENTRY_FG
-ENTRY_RELIEF = config.ENTRY_RELIEF
-FG = config.FG
-FONT = config.FONT
+from src.config import config
 
 
 class Frame(tk.Frame):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, bg=BG, **kwargs)
+        super().__init__(*args, bg=config.BG, **kwargs)
 
 
 class LabelFrame(tk.LabelFrame):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, bg=BG, fg=FG, font=FONT, **kwargs)
+        super().__init__(*args, bg=config.BG, fg=config.FG, font=config.FONT, **kwargs)
 
 
 class Label(tk.Label):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, bg=BG, fg=FG, font=FONT, **kwargs)
+        super().__init__(*args, bg=config.BG, fg=config.FG, font=config.FONT, **kwargs)
 
 
 class Scrollbar(tk.Scrollbar):
@@ -54,9 +39,9 @@ class Scrollbar(tk.Scrollbar):
         super().__init__(
             *args,
             bd=0,
-            relief=ENTRY_RELIEF,
-            bg=ENTRY_BG,
-            activebackground=ENTRY_BG,
+            relief=config.ENTRY_RELIEF,
+            bg=config.ENTRY_BG,
+            activebackground=config.ENTRY_BG,
             **kwargs
         )
 
@@ -65,10 +50,10 @@ class Checkbutton(tk.Checkbutton):
     def __init__(self, *args, **kwargs):
         super().__init__(
             *args,
-            bg=BG,
-            fg=ENTRY_FG,
-            relief=ENTRY_RELIEF,
-            selectcolor=ENTRY_BG,
+            bg=config.BG,
+            fg=config.ENTRY_FG,
+            relief=config.ENTRY_RELIEF,
+            selectcolor=config.ENTRY_BG,
             **kwargs
         )
 
@@ -77,11 +62,11 @@ class Entry(tk.Entry):
     def __init__(self, *args, **kwargs):
         super().__init__(
             *args,
-            font=FONT,
-            readonlybackground=ENTRY_BG,
-            relief=ENTRY_RELIEF,
-            bg=ENTRY_BG,
-            fg=ENTRY_FG,
+            font=config.FONT,
+            readonlybackground=config.ENTRY_BG,
+            relief=config.ENTRY_RELIEF,
+            bg=config.ENTRY_BG,
+            fg=config.ENTRY_FG,
             **kwargs
         )
 
@@ -90,12 +75,12 @@ class Button(tk.Button):
     def __init__(self, *args, **kwargs):
         super().__init__(
             *args,
-            activebackground=BTTN_ACTIVE_BG,
-            activeforeground=BTTN_ACTIVE_FG,
-            relief=BTTN_RELIEF,
-            font=(BTTN_FONT, BTTN_FONT_SIZE),
-            bg=BTTN_BG,
-            fg=BTTN_FG,
+            activebackground=config.BTTN_ACTIVE_BG,
+            activeforeground=config.BTTN_ACTIVE_FG,
+            relief=config.BTTN_RELIEF,
+            font=(config.BTTN_FONT, config.BTTN_FONT_SIZE),
+            bg=config.BTTN_BG,
+            fg=config.BTTN_FG,
             **kwargs
         )
 
@@ -104,11 +89,11 @@ class DeleteButton(tk.Button):
     def __init__(self, *args, **kwargs):
         super().__init__(
             *args,
-            activebackground=DLT_BTTN_ACTIVE_BG,
-            activeforeground=BTTN_ACTIVE_FG,
-            relief=BTTN_RELIEF,
-            font=(BTTN_FONT, BTTN_FONT_SIZE),
-            bg=DLT_BTTN_BG,
-            fg=BTTN_FG,
+            activebackground=config.DLT_BTTN_ACTIVE_BG,
+            activeforeground=config.BTTN_ACTIVE_FG,
+            relief=config.BTTN_RELIEF,
+            font=(config.BTTN_FONT, config.BTTN_FONT_SIZE),
+            bg=config.DLT_BTTN_BG,
+            fg=config.BTTN_FG,
             **kwargs
         )
