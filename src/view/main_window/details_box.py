9fce264937dcc76669277b44d7651d7079df9e7b
tralph3 <tomasralph2000@gmail.com>
2024-05-13 22:23:39 -0300

    Abstract widget creation

--- src/view/main_window/details_box.py
+++ src/view/main_window/details_box.py
@@ -6,64 +6,64 @@ class DetailsBox(Gtk.Box):
     def __init__(self, model, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model = model
-
         self.currently_loaded_app = None
+        self._make_widgets()
+        self._connect_signals()
 
+    def _make_widgets(self):
         self.set_orientation(Gtk.Orientation.VERTICAL)
         self.set_spacing(15)
-        id_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, hexpand=True, spacing=5)
-        id_label = Gtk.Label(label="App ID", vexpand=False, halign=Gtk.Align.START)
-        id_label.set_css_classes(['entry_title'])
-        self.id_entry = Gtk.Entry(vexpand=False, hexpand=True)
-        id_box.append(id_label)
-        id_box.append(self.id_entry)
+
+        id_box, self.id_entry = self._compose_entry_box("App ID", editable=False)
         self.append(id_box)
 
-        name_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, hexpand=True, spacing=5)
-        name_label = Gtk.Label(label="Name", vexpand=False, halign=Gtk.Align.START)
-        name_label.set_css_classes(['entry_title'])
-        self.name_entry = Gtk.Entry(vexpand=False, hexpand=True)
-        self.name_entry.connect("changed", self._update_app_name)
-        name_box.append(name_label)
-        name_box.append(self.name_entry)
+        name_box, self.name_entry = self._compose_entry_box("Name")
         self.append(name_box)
 
-        sortas_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, hexpand=True, spacing=5)
-        sortas_label = Gtk.Label(label="Sort as", vexpand=False, halign=Gtk.Align.START)
-        sortas_label.set_css_classes(['entry_title'])
-        self.sortas_entry = Gtk.Entry(vexpand=False, hexpand=True)
-        self.sortas_entry.set_placeholder_text("Unspecified")
-        sortas_box.append(sortas_label)
-        sortas_box.append(self.sortas_entry)
+        sortas_box, self.sortas_entry = self._compose_entry_box("Sort as", "Unspecified")
         self.append(sortas_box)
 
-        steam_release_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, hexpand=True, spacing=5)
-        steam_release_label = Gtk.Label(label="Steam release date", vexpand=False, halign=Gtk.Align.START)
-        steam_release_label.set_css_classes(['entry_title'])
-        self.steam_release_entry = Gtk.Entry(vexpand=False, hexpand=True)
-        self.steam_release_entry.set_editable(False)
-        self.steam_release_entry.set_can_focus(False)
-        self.steam_release_entry.set_placeholder_text("Unspecified")
-        self.steam_release_entry.set_icon_from_icon_name(Gtk.EntryIconPosition.SECONDARY, "x-office-calendar")
-        self.steam_release_entry.connect("icon-press", self.show_calendar_popover)
-        steam_release_box.append(steam_release_label)
-        steam_release_box.append(self.steam_release_entry)
+        steam_release_box, self.steam_release_entry = self._compose_entry_box(
+            "Steam release date", "Unspecified", False, "x-office-calendar")
         self.append(steam_release_box)
 
-        original_release_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, hexpand=True, spacing=5)
-        original_release_label = Gtk.Label(label="Original release date", vexpand=False, halign=Gtk.Align.START)
-        original_release_label.set_css_classes(['entry_title'])
-        self.original_release_entry = Gtk.Entry(vexpand=False, hexpand=True)
-        self.original_release_entry.set_editable(False)
-        self.original_release_entry.set_can_focus(False)
-        self.original_release_entry.set_placeholder_text("Unspecified")
-        self.original_release_entry.set_icon_from_icon_name(Gtk.EntryIconPosition.SECONDARY, "x-office-calendar")
-        self.original_release_entry.connect("icon-press", self.show_calendar_popover)
-        original_release_box.append(original_release_label)
-        original_release_box.append(self.original_release_entry)
+        original_release_box, self.original_release_entry = self._compose_entry_box(
+            "Original release date", "Unspecified", False, "x-office-calendar")
         self.append(original_release_box)
 
-    def show_calendar_popover(self, entry, icon_position) -> None:
+    @staticmethod
+    def _make_label(label: str) -> Gtk.Label:
+        label = Gtk.Label(label=label, vexpand=False, halign=Gtk.Align.START)
+        label.set_css_classes(['entry_title'])
+        return label
+
+    @staticmethod
+    def _make_entry(placeholder: str=None, editable: bool=True, icon: str=None) -> Gtk.Entry:
+        entry = Gtk.Entry(vexpand=False, hexpand=True)
+        if not editable:
+            entry.set_editable(False)
+            entry.set_can_focus(False)
+        if placeholder:
+            entry.set_placeholder_text(placeholder)
+        if icon:
+            entry.set_icon_from_icon_name(Gtk.EntryIconPosition.SECONDARY, icon)
+        return entry
+
+    @staticmethod
+    def _make_box() -> Gtk.Box:
+        return Gtk.Box(orientation=Gtk.Orientation.VERTICAL, hexpand=True, spacing=5)
+
+    def _compose_entry_box(self, label: str, placeholder: str=None, editable: bool=True, icon: str=None) -> (Gtk.Box, Gtk.Entry):
+        box = self._make_box()
+        label = self._make_label(label)
+        entry = self._make_entry(placeholder, editable, icon)
+        box.append(label)
+        box.append(entry)
+        return (box, entry)
+
+    def _connect_signals(self):
+        self.steam_release_entry.connect("icon-press", self.show_calendar_popover)
+        self.original_release_entry.connect("icon-press", self.show_calendar_popover)
         popover = Gtk.Popover()
         popover.set_parent(entry)
         calendar = Gtk.Calendar()
