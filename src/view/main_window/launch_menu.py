a8096fb338669f39642d3e710e0581d202014a8c
tralph3 <tomasralph2000@gmail.com>
2024-06-04 15:42:02 -0300

    add os checkbuttons

--- src/view/main_window/launch_menu.py
+++ src/view/main_window/launch_menu.py
@@ -1,4 +1,4 @@
-from gi.repository import Gtk, Gdk
+from gi.repository import Gtk, Adw
 from view.objects import App
 from view.events import Event, event_connect
 from .util import compose_entry_box, _make_box
@@ -13,14 +13,31 @@ class LaunchEntry(Gtk.Box):
 
     def _make_widgets(self):
         self.set_orientation(Gtk.Orientation.VERTICAL)
-        desc_box, self.desc_entry = compose_entry_box("Description", "Unspecified")
-        exec_box, self.exec_entry = compose_entry_box("Executable", "Unspecified", False, "folder")
+        description_box, self.description_entry = compose_entry_box("Description", "Unspecified")
+        executable_box, self.execuable_entry = compose_entry_box("Executable", "Unspecified", False, "folder")
         workdir_box, self.workdir_entry = compose_entry_box("Working Directory", "Unspecified", False, "folder")
-        args_box, self.args_entry = compose_entry_box("Launch Arguments", "Unspecified")
-        self.append(desc_box)
-        self.append(exec_box)
+        arguments_box, self.arguments_entry = compose_entry_box("Launch Arguments", "Unspecified")
+        delete_button = Gtk.Button()
+        bottom_box = Gtk.Box()
+        checkbutton_box = Gtk.Box(hexpand=True)
+        self.windows_checkbutton = Gtk.CheckButton(label="Windows")
+        self.mac_checkbutton = Gtk.CheckButton(label="Mac")
+        self.linux_checkbutton = Gtk.CheckButton(label="Linux")
+
+        delete_button.set_icon_name("edit-delete")
+
+        checkbutton_box.append(self.windows_checkbutton)
+        checkbutton_box.append(self.mac_checkbutton)
+        checkbutton_box.append(self.linux_checkbutton)
+        bottom_box.append(checkbutton_box)
+        bottom_box.append(delete_button)
+        bottom_box.set_hexpand(True)
+
+        self.append(description_box)
+        self.append(executable_box)
         self.append(workdir_box)
-        self.append(args_box)
+        self.append(arguments_box)
+        self.append(bottom_box)
 
     def _configure_widgets(self):
         css_class = ""
@@ -30,10 +47,17 @@ class LaunchEntry(Gtk.Box):
             css_class = "launch_entry_odd"
         self.set_spacing(15)
         self.set_css_classes([css_class])
-        self.desc_entry.set_text(self.entry.get("description", ""))
-        self.exec_entry.set_text(self.entry.get("executable", ""))
+        self.description_entry.set_text(self.entry.get("description", ""))
+        self.execuable_entry.set_text(self.entry.get("executable", ""))
         self.workdir_entry.set_text(self.entry.get("workingdir", ""))
-        self.args_entry.set_text(self.entry.get("arguments", ""))
+        arguments = self.entry.get("arguments", "")
+        if isinstance(arguments, int): arguments = ""
+        self.arguments_entry.set_text(arguments)
+
+        entry_oslist = self.entry.get("config", {}).get("oslist", "").lower()
+        self.windows_checkbutton.set_active("windows" in entry_oslist)
+        self.mac_checkbutton.set_active("macos" in entry_oslist)
+        self.linux_checkbutton.set_active("linux" in entry_oslist)
 
 
 class LaunchMenu(Gtk.Frame):
@@ -48,13 +72,18 @@ class LaunchMenu(Gtk.Frame):
 
     def _make_widgets(self):
         scrolled_window = Gtk.ScrolledWindow()
-        self._main_box = _make_box()
+        self._entries_box = _make_box()
+        tool_bar = Adw.ToolbarView()
+        action_bar = Gtk.ActionBar()
 
-        self.set_child(scrolled_window)
-        scrolled_window.set_child(self._main_box)
+        add_button = Gtk.Button()
+        add_button.set_icon_name("list-add")
+        action_bar.pack_end(add_button)
+        tool_bar.add_bottom_bar(action_bar)
+        tool_bar.set_content(scrolled_window)
 
-        for entry in self._make_entries():
-            self._main_box.append(entry)
+        self.set_child(tool_bar)
+        scrolled_window.set_child(self._entries_box)
 
     def _connect_signals(self):
         event_connect(Event.LOAD_APP, self._load_app)
@@ -68,6 +97,17 @@ class LaunchMenu(Gtk.Frame):
             entries.append(LaunchEntry(app_entries[entry], i))
         return entries
 
+    def _delete_all_entries_from_box(self):
+        child = self._entries_box.get_first_child()
+        while child:
+            self._entries_box.remove(child)
+            child = self._entries_box.get_first_child()
+
+    def _load_app_entries(self):
+        self._delete_all_entries_from_box()
+        for entry in self._make_entries():
+            self._entries_box.append(entry)
+
     def _load_app(self, app: App):
         self._current_app = app
-        self._make_widgets()
+        self._load_app_entries()
