4554ff2262e2d7befc8c931b1502bd75d3337349
tralph3 <tomasralph2000@gmail.com>
2024-06-12 23:28:05 -0300

    mark attributes as private

--- src/view/main_window/launch_menu.py
+++ src/view/main_window/launch_menu.py
@@ -7,30 +7,30 @@ class LaunchEntry(Gtk.Box):
     def __init__(self, entry: dict, appid: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.appid = appid
-        self.entry = entry
+        self._entry = entry
         self._make_widgets()
         self._configure_widgets()
 
     def _make_widgets(self):
         self.set_orientation(Gtk.Orientation.VERTICAL)
-        description_box, self.description_entry = compose_entry_box("Description", "Unspecified")
-        executable_box, self.executable_entry = compose_entry_box("Executable", "Unspecified", False, "folder")
-        workdir_box, self.workdir_entry = compose_entry_box("Working Directory", "Unspecified", False, "folder")
-        arguments_box, self.arguments_entry = compose_entry_box("Launch Arguments", "Unspecified")
+        description_box, self._description_entry = compose_entry_box("Description", "Unspecified")
+        executable_box, self._executable_entry = compose_entry_box("Executable", "Unspecified", False, "folder")
+        workdir_box, self._workdir_entry = compose_entry_box("Working Directory", "Unspecified", False, "folder")
+        arguments_box, self._arguments_entry = compose_entry_box("Launch Arguments", "Unspecified")
         delete_button = Gtk.Button()
         bottom_box = Gtk.Box()
         checkbutton_box = Gtk.Box(hexpand=True)
-        self.windows_checkbutton = Gtk.CheckButton(label="Windows")
-        self.mac_checkbutton = Gtk.CheckButton(label="Mac")
-        self.linux_checkbutton = Gtk.CheckButton(label="Linux")
+        self._windows_checkbutton = Gtk.CheckButton(label="Windows")
+        self._mac_checkbutton = Gtk.CheckButton(label="Mac")
+        self._linux_checkbutton = Gtk.CheckButton(label="Linux")
 
         delete_button.set_icon_name("edit-delete")
         delete_button.connect("clicked", lambda *_: self._delete_self())
         delete_button.set_css_classes(["button", "delete_button"])
 
-        checkbutton_box.append(self.windows_checkbutton)
-        checkbutton_box.append(self.mac_checkbutton)
-        checkbutton_box.append(self.linux_checkbutton)
+        checkbutton_box.append(self._windows_checkbutton)
+        checkbutton_box.append(self._mac_checkbutton)
+        checkbutton_box.append(self._linux_checkbutton)
         bottom_box.append(checkbutton_box)
         bottom_box.append(delete_button)
         bottom_box.set_hexpand(True)
@@ -43,45 +43,45 @@ class LaunchEntry(Gtk.Box):
 
     def _configure_widgets(self):
         self.set_spacing(15)
-        self.description_entry.set_text(self.entry.get("description", ""))
-        self.executable_entry.set_text(self.entry.get("executable", ""))
-        self.workdir_entry.set_text(self.entry.get("workingdir", ""))
-        arguments = self.entry.get("arguments", "")
+        self._description_entry.set_text(self._entry.get("description", ""))
+        self._executable_entry.set_text(self._entry.get("executable", ""))
+        self._workdir_entry.set_text(self._entry.get("workingdir", ""))
+        arguments = self._entry.get("arguments", "")
         if isinstance(arguments, int): arguments = ""
-        self.arguments_entry.set_text(arguments)
+        self._arguments_entry.set_text(arguments)
 
-        entry_oslist = self.entry.get("config", {}).get("oslist", "").lower()
-        self.windows_checkbutton.set_active("windows" in entry_oslist)
-        self.mac_checkbutton.set_active("macos" in entry_oslist)
-        self.linux_checkbutton.set_active("linux" in entry_oslist)
+        entry_oslist = self._entry.get("config", {}).get("oslist", "").lower()
+        self._windows_checkbutton.set_active("windows" in entry_oslist)
+        self._mac_checkbutton.set_active("macos" in entry_oslist)
+        self._linux_checkbutton.set_active("linux" in entry_oslist)
 
     def _delete_self(self):
         event_emit(Event.DELETE_LAUNCH_ENTRY, self)
 
     def _make_oslist_string(self) -> str:
         selected_os = []
-        if self.windows_checkbutton.get_active():
+        if self._windows_checkbutton.get_active():
             selected_os.append("windows")
-        if self.mac_checkbutton.get_active():
+        if self._mac_checkbutton.get_active():
             selected_os.append("macos")
-        if self.linux_checkbutton.get_active():
+        if self._linux_checkbutton.get_active():
             selected_os.append("linux")
         return ','.join(selected_os)
 
     def get_updated_entry(self) -> dict:
-        if self.description_entry.get_text():
-            self.entry["description"] = self.description_entry.get_text()
-        if self.executable_entry.get_text():
-            self.entry["executable"] = self.executable_entry.get_text()
-        if self.workdir_entry.get_text():
-            self.entry["workingdir"] = self.workdir_entry.get_text()
-        if self.arguments_entry.get_text():
-            self.entry["arguments"] = self.arguments_entry.get_text()
+        if self._description_entry.get_text():
+            self._entry["description"] = self._description_entry.get_text()
+        if self._executable_entry.get_text():
+            self._entry["executable"] = self._executable_entry.get_text()
+        if self._workdir_entry.get_text():
+            self._entry["workingdir"] = self._workdir_entry.get_text()
+        if self._arguments_entry.get_text():
+            self._entry["arguments"] = self._arguments_entry.get_text()
         oslist = self._make_oslist_string()
         if oslist:
-            self.entry.setdefault("config", {})
-            self.entry["config"]["oslist"] = oslist
-        return self.entry
+            self._entry.setdefault("config", {})
+            self._entry["config"]["oslist"] = oslist
+        return self._entry
 
 
 class LaunchMenu(Gtk.Frame):
