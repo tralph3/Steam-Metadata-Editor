6558fd729bba8f4e435dc50c373640d1b739404d
tralph3 <tomasralph2000@gmail.com>
2024-06-16 01:07:56 -0300

    store indent state in encoder class

--- src/textvdf/encoder.py
+++ src/textvdf/encoder.py
@@ -3,20 +3,20 @@ import json
 
 class TextVdfEncoder:
     def __init__(self, obj: dict=None):
-        self.obj = obj
+        self._obj = obj
+        self._indent = 0
 
-    def encode(self, obj=None, indent=0) -> str:
+    def encode(self, obj: dict=None) -> str:
         result = ""
-        tabs = "\t" * indent
-        if obj is None:
-            obj = self.obj
-        for key in obj.keys():
+        tabs = "\t" * self._indent
+        if obj is None: obj = self._obj
+        for key in obj:
             if isinstance(obj[key], dict):
-                indent += 1
-                value = self.encode(obj[key], indent)
+                self._indent += 1
+                value = self.encode(obj[key])
                 key = key.replace("\\", "\\\\")
                 result += f'{tabs}"{key}"\n{tabs}{"{\n"}{value}{tabs}{"}\n"}'
-                indent -= 1
+                self._indent -= 1
             else:
                 value = str(obj[key]).replace("\\", "\\\\")
                 key = key.replace("\\", "\\\\")
